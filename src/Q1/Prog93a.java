ae8b9305397d9da172e67800db02de4ddf661e27
purkapile.s <sjpurkapile@gmail.com>
2024-09-16 11:02:23 -0500

    :)

--- src/Q1/Prog93a.java
+++ src/Q1/Prog93a.java
@@ -6,5 +6,31 @@ public class Prog93a {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter kilowatts used: ");
         int kilo = input.nextInt();
+        double br = kilo * 0.0475;
+        double sc = br * 0.1;
+        double ct = br * 0.03;
+
+        System.out.printf("Base Rate: $%.2f\n", br);
+        System.out.printf("Surcharge: $%.2f\n", sc);
+        System.out.printf("Citytax: $%.2f\n", ct);
+        System.out.println("");
+        double rbr = Math.round(br * 100.0) / 100.0;
+        double rct = Math.round(ct * 100.0) / 100.0;
+        double rsc = Math.round(sc * 100.0) / 100.0;
+
+        double pay = rbr + rct + rsc;
+        double lpay = pay * 1.04;
+
+        System.out.printf("Pay: $%.2f\n", pay);
+        System.out.printf("Late Payment Cost: $%.2f\n", lpay);
     }
 }
+/*
+Enter kilowatts used: 993
+Base Rate: $47.17
+Surcharge: $4.72
+Citytax: $1.42
+
+Pay: $53.31
+Late Payment Cost: $55.44
+ */
