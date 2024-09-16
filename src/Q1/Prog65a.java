0d09c7541a1832bc175f997ac987cc7408ad12fe
purkapile.s <sjpurkapile@gmail.com>
2024-09-16 11:26:06 -0500

    :)

---
+++ src/Q1/Prog65a.java
@@ -0,0 +1,13 @@
+package Q1;
+import java.util.*;
+
+public class Prog65a {
+    public static void main(String[] args) {
+        // Sorry Sydney, I hate Java and cannot figure it out. Although this makes me very sad, I regret to inform you that
+        // I am a complete and utter fool and cannot even begin to fathom String and int arrays. It's so much worse than
+        // It works in Python and I just hate it completely so much.
+        // JAVA IS HORRIBLe.
+        // Save me from Java and return me to the virtues of Python so that I can be happy and free again.
+        // I chose it so I could have a better future in college and be able to graduate earlier and save money.
+    }
+}
