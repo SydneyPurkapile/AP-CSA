384bdcf359c47047ae621cacdb1720c3d9448732
purkapile.s <sjpurkapile@gmail.com>
2024-09-19 11:00:54 -0500

    :)

--- src/Q1/Prog82a.java
+++ src/Q1/Prog82a.java
@@ -4,6 +4,18 @@ import java.util.*;
 public class Prog82a {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.print("Enter Speed Limit: ");
+        int sl = input.nextInt();
+        System.out.print("Enter Vehicle Speed: ");
+        int vs = input.nextInt();
+        int dif = vs - sl;
+        int tfc = dif * 5;
+        int ticket = 20 + tfc;
+        System.out.println("Fine: $" + ticket);
     }
 }
+/*
+Enter Speed Limit: 30
+Enter Vehicle Speed: 42
+Fine: $80
+ */
