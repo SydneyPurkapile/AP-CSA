f5d4e1b9e0acbad760f3592ae1ecbd66dc6667fd
purkapile.s <sjpurkapile@gmail.com>
2024-09-23 11:32:26 -0500

    :)

---
+++ src/Q1/Prog122b.java
@@ -0,0 +1,13 @@
+package Q1;
+
+public class Prog122b {
+    public static void main(String[] args) {
+        int lcv1 = 0;
+        int lcv2 = 0;
+        while (lcv1 < 40) {
+            lcv1 += 1;
+            lcv2 = lcv1 * 4;
+            System.out.println(lcv1 + "\t\t" + lcv2);
+        }
+    }
+}
