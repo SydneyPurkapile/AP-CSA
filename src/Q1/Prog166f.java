e9871f94edb8f2081776ca7bf51a9204b1a522cc
purkapile.s <sjpurkapile@gmail.com>
2024-09-25 10:36:58 -0500

    :)

---
+++ src/Q1/Prog166f.java
@@ -0,0 +1,50 @@
+package Q1;
+
+public class Prog166f {
+    public static void main(String[] args) {
+        int lcv1 = 0;
+        int lcv2 = 0;
+        while (lcv1 < 100) {
+            lcv1 += 1;
+            lcv2 += lcv1;
+        }
+        System.out.println("The sum of the numbers 1-100: " + lcv2);
+        lcv2 += 101;
+        System.out.println("The sum of the numbers 1-101: " + lcv2);
+        lcv2 += 102;
+        System.out.println("The sum of the numbers 1-102: " + lcv2);
+        lcv2 += 103;
+        System.out.println("The sum of the numbers 1-103: " + lcv2);
+        lcv2 += 104;
+        System.out.println("The sum of the numbers 1-104: " + lcv2);
+        lcv2 += 104;
+        System.out.println("The sum of the numbers 1-105: " + lcv2);
+        lcv2 += 105;
+        System.out.println("The sum of the numbers 1-106: " + lcv2);
+        lcv2 += 106;
+        System.out.println("The sum of the numbers 1-107: " + lcv2);
+        lcv2 += 107;
+        System.out.println("The sum of the numbers 1-108: " + lcv2);
+        lcv2 += 108;
+        System.out.println("The sum of the numbers 1-109: " + lcv2);
+        lcv2 += 109;
+        System.out.println("The sum of the numbers 1-110: " + lcv2);
+        lcv2 += 110;
+        System.out.println("The sum of the numbers 1-111: " + lcv2);
+
+    }
+}
+/*
+The sum of the numbers 1-100: 5050
+The sum of the numbers 1-101: 5151
+The sum of the numbers 1-102: 5253
+The sum of the numbers 1-103: 5356
+The sum of the numbers 1-104: 5460
+The sum of the numbers 1-105: 5564
+The sum of the numbers 1-106: 5669
+The sum of the numbers 1-107: 5775
+The sum of the numbers 1-108: 5882
+The sum of the numbers 1-109: 5990
+The sum of the numbers 1-110: 6099
+The sum of the numbers 1-111: 6209
+ */
