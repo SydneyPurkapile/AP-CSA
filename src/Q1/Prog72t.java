dc8d1ceefb3a24652027db4429689d1bca85fc24
purkapile.s <sjpurkapile@gmail.com>
2024-10-03 10:17:46 -0500

    :)

--- src/Q1/Prog72t.java
+++ src/Q1/Prog72t.java
@@ -1,7 +1,24 @@
 package Q1;
-
+import java.util.Scanner;
 public class Prog72t {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first time: ");
+        int ftime = input.nextInt();
+        System.out.print("Enter the second time: ");
+        int stime = input.nextInt();
+        int timef = ftime / 100;
+        int times = stime / 100;
+        int hours = Math.abs(timef - times);
+        System.out.print(hours + "hours and ");
+        int min = ftime - stime;
+        hours *= 100;
+        min = Math.abs(min + hours);
+        System.out.print(min + " minutes");
     }
 }
+/*
+Enter the first time: 900
+Enter the second time: 1730
+8hours and 30 minutes
+ */
--- src/Q1/StringInt6.java
+++ src/Q1/StringInt6.java
@@ -5,16 +5,18 @@ public class StringInt6 {
     public static void main(String[] args) {
         Scanner wow = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String text = wow.nextLine().toLowerCase().trim();
+        String str = wow.nextLine().toLowerCase().trim();
+        System.out.print("Not a duplicate letter: ");
 
-        int ACount = 0;
-        int BCount = 0;
-        int CCount = 0;
-
-        for (int lcv = 0; lcv < text.length(); lcv++) {
-            // Maybe substring will work better than char?
-            char cur = text.charAt(lcv);
-
+        for (int lcv = 0; lcv < str.length(); lcv++) {
+            for (int lcv2 = lcv + 1; lcv2 < str.length(); lcv2++) {
+                String let1 = str.substring(lcv, lcv + 1);
+                String let2 = str.substring(lcv2, lcv2 + 1);
+                if (!let1.equals(let2)) {
+                    System.out.print(let2 + " ");
+                    break;
+                }
+            }
         }
     }
 }
