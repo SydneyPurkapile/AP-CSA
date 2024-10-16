cace1039802c40d5d6fb2e2ce431cad341a81654
purkapile.s <sjpurkapile@gmail.com>
2024-10-16 11:12:20 -0500

    :)

--- src/Q1/LP5_6.java
+++ src/Q1/LP5_6.java
@@ -1,12 +1,11 @@
 package Q1;
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class LP5_6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a positive integer: ");
         int num = input.nextInt();
+
     }
 }
--- src/Q1/Prog93aClass/Prog93aClass.java
+++ src/Q1/Prog93aClass/Prog93aClass.java
@@ -1,7 +1,9 @@
 package Q1.Prog93aClass;
+import java.util.Scanner;
 
 public class Prog93aClass {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
     }
 }
