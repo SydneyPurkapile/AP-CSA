b52c88727e39aad6b86a419b05fa48f01ebfab97
purkapile.s <sjpurkapile@gmail.com>
2024-11-05 11:24:26 -0600

    :)

---
+++ src/Prog213fArray/Class213f.java
@@ -0,0 +1,19 @@
+package Prog213fArray;
+
+public class Class213f {
+    private double myKWH;
+    private double myCost;
+
+    public Class213f(double kwh) {
+        myKWH = kwh;
+        myCost = 0;
+    }
+
+    public void calc() { // TODO: complete
+
+    }
+
+    public String toString() {
+        return("The cost of "); // TODO: complete
+    }
+}
---
+++ src/Prog213fArray/Prog213fArray.java
@@ -0,0 +1,33 @@
+package Prog213fArray;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog213fArray {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/prog213f.dat"));
+
+            Class213f[] list = new Class213f[1000];
+            int cnt = 0;
+
+            while (file.hasNext()) {
+                int kwh = file.nextInt();
+
+                if (kwh != -999) {
+                    Class213f yikes = new Class213f(kwh);
+                    list[cnt] = yikes;
+                    cnt++;
+                }
+            }
+
+            for (int lcv = 0; lcv < cnt; lcv++) {
+                list[lcv].calc();
+                System.out.println(list[lcv]);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
