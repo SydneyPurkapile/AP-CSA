55ca7476cb10823710b17116f2d7736353f4af33
purkapile.s <sjpurkapile@gmail.com>
2024-11-08 10:56:48 -0600

    :)

--- src/Q2/Prog214b/Class214b.java
+++ src/Q2/Prog214b/Class214b.java
@@ -2,5 +2,10 @@ package Q2.Prog214b;
 
 public class Class214b {
     private int myNum;
-    private int
+    private int myPay;
+    private int myBPR;
+    private int myShiftCode;
+    private int myHrWork;
+
+
 }
