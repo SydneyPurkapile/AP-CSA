5cdf4c1a07a527c536f7bcc2c48891dc71321278
purkapile.s <sjpurkapile@gmail.com>
2024-11-13 11:24:57 -0600

    :)

--- src/Q2/MSOE_2016/MSOE_2016_1.java
+++ src/Q2/MSOE_2016/MSOE_2016_1.java
@@ -1,4 +1,11 @@
 package Q2.MSOE_2016;
+import java.util.Scanner;
 
 public class MSOE_2016_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a lowercase word: ");
+        String word = input.nextLine();
+
+    }
 }
