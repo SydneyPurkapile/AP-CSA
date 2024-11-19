67fc6be6716d98ac9bbd0dc66b3845a8f0cc089d
purkapile.s <sjpurkapile@gmail.com>
2024-11-19 11:15:20 -0600

    :)

--- src/Q2/MSOE_2016/MSOE_2016_1.java
+++ src/Q2/MSOE_2016/MSOE_2016_1.java
@@ -6,6 +6,10 @@ public class MSOE_2016_1 {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a lowercase word: ");
         String word = input.nextLine();
-
+        for (int k = 0; k < word.length(); k++) {
+            if (word.substring(k, k+1) == word.substring(word.length()-k))
+                System.out.println("Yes");
+        }
+        System.out.println("No");
     }
 }
