9a5b8fbd0f86b6a138f2009cab6dc6b404385ffb
purkapile.s <sjpurkapile@gmail.com>
2024-11-26 11:08:53 -0600

    :)

--- src/Q2/Prog435aClass/Class435a.java
+++ src/Q2/Prog435aClass/Class435a.java
@@ -1,4 +1,48 @@
 package Q2.Prog435aClass;
 
 public class Class435a {
+    private int myCar;
+    private int myGate;
+    private double myToll;
+    private double myFactor;
+    private double myCost;
+
+    public Class435a(int car, int gate) {
+        myCar = car;
+        myGate = gate;
+        myToll = 0.0;
+        myFactor = 0.0;
+        myCost = 0.0;
+    }
+
+    public double getToll() {
+        if (myGate == 1) myToll = 1.35;
+        if (myGate == 2) myToll = 2.00;
+        if (myGate == 3) myToll = 2.50;
+        if (myGate == 4) myToll = 3.25;
+        if (myGate == 5) myToll = 4.10;
+        if (myGate == 6) myToll = 4.80;
+        if (myGate == 7) myToll = 5.50;
+        if (myGate == 8) myToll = 6.00;
+        return myToll;
+    }
+
+    public double getFactor() {
+        if (myCar == 1) myFactor = 1.0;
+        if (myCar == 2) myFactor = 1.3;
+        if (myCar == 3) myFactor = 1.6;
+        if (myCar == 4) myFactor = 2.0;
+        if (myCar == 5) myFactor = 2.4;
+        if (myCar == 6) myFactor = 2.7;
+        return myFactor;
+    }
+
+    public double getCost() {
+        myCost = myFactor * myToll;
+        return myCost;
+    }
+
+    public String toString() {
+        return myCar + "\t\t" + myToll + "\t\t" + myFactor + "\t\t$" + myCost;
+    }
 }
--- src/Q2/Prog435aClass/Prog435aClass.java
+++ src/Q2/Prog435aClass/Prog435aClass.java
@@ -1,4 +1,6 @@
 package Q2.Prog435aClass;
+import Q2.Prog402a.Class402a;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -6,10 +8,17 @@ import java.util.Scanner;
 public class Prog435aClass {
     public static void main(String[] args) {
         try {
-            Scanner file = new Scanner(new File("Langdat/FILENAME"));
-
+            Scanner file = new Scanner(new File("Langdat/prog435a.dat"));
+            System.out.println("Car Type\tBase Toll\tFactor\tCost");
             while (file.hasNext()) {
-
+                int car = file.nextInt();
+                int gate = file.nextInt();
+                Class435a stuff = new Class435a(car, gate);
+                stuff.getToll();
+                stuff.getFactor();
+                stuff.getCost();
+                stuff.toString();
+                System.out.println(stuff);
             }
 
         } catch (IOException e) {
@@ -17,3 +26,24 @@ public class Prog435aClass {
         }
     }
 }
+/*
+Car Type	Base Toll	Factor	Cost
+1		1.35		1.0		$1.35
+2		2.5		1.3		$3.25
+3		4.1		1.6		$6.56
+4		5.5		2.0		$11.0
+5		2.0		2.4		$4.8
+6		3.25		2.7		$8.775
+1		4.8		1.0		$4.8
+2		6.0		1.3		$7.800000000000001
+3		1.35		1.6		$2.16
+4		2.5		2.0		$5.0
+5		4.1		2.4		$9.839999999999998
+6		5.5		2.7		$14.850000000000001
+1		6.0		1.0		$6.0
+2		1.35		1.3		$1.7550000000000001
+3		2.0		1.6		$3.2
+4		2.5		2.0		$5.0
+5		3.25		2.4		$7.8
+6		4.1		2.7		$11.07
+ */
