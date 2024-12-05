f0509c50af27fd7dd9addc2f1087398c1dd3f307
purkapile.s <sjpurkapile@gmail.com>
2024-12-05 11:25:05 -0600

    :)

---
+++ src/Q2/LP3_10Class/LP3_10.java
@@ -0,0 +1,12 @@
+package Q2.LP3_10Class;
+import java.util.Scanner;
+
+public class LP3_10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of burgers: ");
+        System.out.println("Enter the number of fries: ");
+        System.out.println("Enter the number of sodas: ");
+
+    }
+}
---
+++ src/Q2/LP3_10Class/LP3_10Class.java
@@ -0,0 +1,23 @@
+package Q2.LP3_10Class;
+
+public class LP3_10Class {
+    private double myBC;
+    private double myFC;
+    private double mySC;
+    private double mytbt;
+    private double myTax;
+    private double myFT;
+
+    public ClassLP310(double BC, double FC, double SC) {
+        myBC = BC;
+        myFC = FC;
+        mySC = SC;
+        mytbt = 0;
+        myTax = 0;
+        myFT = 0;
+    }
+
+    public getTax() {
+
+    }
+}
