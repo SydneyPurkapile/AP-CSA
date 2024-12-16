b9337925cf65b62fe5e8ef0216893dba11ce60b3
purkapile.s <sjpurkapile@gmail.com>
2024-12-16 11:12:02 -0600

    :)

---
+++ src/Q2/StudentDB/StudentDatabase.java
@@ -0,0 +1,90 @@
+package Q2.StudentDB;
+import java.util.*;
+
+public class StudentDatabase {
+    public static int indexOfStu(ArrayList<Student> db, int id) {
+        for (int i = 0; i < db.size(); i++)
+            if (db.get(i).getId() == id)
+                return i;
+        return -1;  // Not Found
+    }
+
+    public static int indexOfStu(ArrayList<Student> db, String last) {
+        for (int i = 0; i < db.size(); i++)
+            if (db.get(i).getLastName().equals(last))
+                return i;
+        return -1;  // Not Found
+    }
+
+    public static void printStu(Student s) {
+        System.out.printf("%s\t%s\t%d\t%.2f\n",
+                s.getFirstName(), s.getLastName(), s.getId(), s.getGPA());
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Student> db = new ArrayList<>();
+
+        while (true) {
+            System.out.println("\nStudent Menu:");
+            System.out.println("1) Add New Student");
+            System.out.println("2) Display All Students");
+            System.out.println("3) Search for Student(by ID)");
+            System.out.println("4) Search for Student(by Last Name)");
+            System.out.println("5) Modify a Student's GPA");
+            System.out.println("6) Remove Student(by ID)");
+            System.out.println("7) Exit");
+            System.out.print("Please Enter Your Choice: ");
+            int c = input.nextInt();
+
+            if (c == 1) {
+                System.out.print("Enter first name: ");
+                String f = input.next();
+                System.out.print("Enter last name: ");
+                String l = input.next();
+                System.out.print("Enter student ID: ");
+                int id = input.nextInt();
+                System.out.print("Enter GPA: ");
+                double gpa = input.nextDouble();
+                Student st = new Student(f, l, id, gpa);
+                db.add(st);
+            } else if (c == 2) {
+                System.out.println("First\tLast\tID\tGPA");
+                for (Student s : db)
+                    printStu(s);
+            } else if (c == 3) {
+                System.out.print("Enter student ID: ");
+                int id = input.nextInt();
+                int index = indexOfStu(db, id);
+                if (index != -1) printStu(db.get(index));
+                else System.out.println("No Student Was Found");
+            } else if (c == 4) {
+                System.out.print("Enter student's last name: ");
+                String last = input.next();
+                int index = indexOfStu(db, last);
+                if (index != -1) printStu(db.get(index));
+                else System.out.println("No Student Was Found");
+            } else if (c == 5) {
+                System.out.print("Enter student ID: ");
+                int id = input.nextInt();
+                int index = indexOfStu(db, id);
+                if (index == -1) System.out.println("No Student Was Found");
+                else {
+                    System.out.print("Enter new GPA: ");
+                    double gpa = input.nextDouble();
+                    db.get(index).setGPA(gpa);
+                    System.out.println("GPA Successfully Updated");
+                }
+            } else if (c == 6) {
+                System.out.print("Enter student ID: ");
+                int id = input.nextInt();
+                int index = indexOfStu(db, id);
+                if (index == -1) System.out.println("No Student Was Found");
+                else db.remove(index);
+            } else if (c == 7) {
+                return;
+            }
+        }
+
+    }
+}
