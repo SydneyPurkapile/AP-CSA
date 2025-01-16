31e983cc3c211d779cacbf92de337a03753fc4b6
purkapile.s <sjpurkapile@gmail.com>
2025-01-16 11:07:36 -0600

    :)

---
+++ src/Q2/LP3_14Class/LP314.java
@@ -0,0 +1,25 @@
+package Q2.LP3_14Class;
+import java.util.Scanner;
+
+public class LP314 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Election Results for New York: ");
+        System.out.print("Awbrey: ");
+        int avy = input.nextInt();
+        System.out.print("Martinez: ");
+        int mvy = input.nextInt();
+        System.out.println("Election Results for New Jersey: ");
+        System.out.print("Awbrey: ");
+        int avj = input.nextInt();
+        System.out.print("Martinez: ");
+        int mvj = input.nextInt();
+        System.out.println("Election Results for Connecticut: ");
+        System.out.print("Awbrey: ");
+        int avc = input.nextInt();
+        System.out.print("Martinez: ");
+        int mvc = input.nextInt();
+
+
+    }
+}
