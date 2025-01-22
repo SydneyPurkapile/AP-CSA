57135f93b50bba643f162a399878cc36140881ba
purkapile.s <sjpurkapile@gmail.com>
2025-01-22 13:55:12 -0600

    :)

--- src/Q2/Prog465c.java
+++ src/Q2/Prog465c.java
@@ -11,9 +11,13 @@ public class Prog465c {
             int[][] mat = new int[file.nextInt()][file.nextInt()];
             int et = 0;
 
-            for (int r = 0; r < mat.length; r++)
-                for (int c = 0; c < mat[0].length-1; c++)
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[0].length; c++) {
                     mat[r][c] = file.nextInt();
+                    if (r == 0 || r == mat.length-1 || c == 0 || c == mat[0].length-1)
+                        et += mat[r][c];
+                }
+            }
 
             for (int[] row : mat) {
                 for (int num : row)
@@ -21,8 +25,19 @@ public class Prog465c {
                 System.out.println();
             }
 
+            System.out.println();
+            System.out.println("Total of the Edges: " + et);
+
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
     }
 }
+
+/*
+100 195 182 225
+83 125 235 67
+129 42 100 750
+
+Total of the Edges: 1873
+ */
