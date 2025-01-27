fa2aefadbfe3165535a1673a44d07e7e54afb4c7
purkapile.s <sjpurkapile@gmail.com>
2025-01-27 11:23:09 -0600

    :)

---
+++ src/Q3/Jeff.java
@@ -0,0 +1,18 @@
+package Q3;
+
+public class Jeff {
+    public static void main(String[] args) {
+        System.out.println("My name is Jeff");
+        String favColor = "green";
+        String favAnimal = "sea otter";
+        String favFood = "french fries";
+        String middleName = "james";
+        String lastName = "szelogowski";
+        String favClass = "algebra one";
+        String favActivity = "torturing his students";
+        String occupation = "high school teacher";
+        String favSport = "golf";
+        String worstFear = "touching grass";
+        String favPastTime = "playing poker";
+    }
+}
