75ace66ce1a7635d984ed89e487e2de9a551e76d
purkapile.s <sjpurkapile@gmail.com>
2025-01-31 11:21:15 -0600

    :)

---
+++ src/Q3/Prog607a.java
@@ -0,0 +1,19 @@
+package Q3;
+import java.util.Scanner;
+
+public class Prog607a {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("1) Encode Date\n2) Decode Date\t3) Quit");
+        System.out.print("Enter Choice: ");
+        int choice = input.nextInt();
+
+        if (choice == 1) {
+
+        } else if (choice == 2) {
+
+        } else if (choice == 3) {
+
+        }
+    }
+}
