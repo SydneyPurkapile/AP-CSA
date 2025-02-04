bcc38bf4d29004649209871a43f232a19e095444
purkapile.s <sjpurkapile@gmail.com>
2025-02-04 11:21:20 -0600

    :)

--- src/Q3/Prog477a.java
+++ src/Q3/Prog477a.java
@@ -8,36 +8,34 @@ public class Prog477a {
         try {
             Scanner file = new Scanner(new File("Langdat/prog477a.dat"));
             int[][] mat = new int[3][6];
-            System.out.println("Male\tFemale\tTotal");
 
             while (file.hasNext()) {
                 if (file.nextInt() == 1) {
                     mat[0][5]++;
                     if (file.nextInt() == 1)
                         mat[0][0]++;
-                    else if (file.nextInt() == 2)
+                    if (file.nextInt() == 2)
                         mat[0][1]++;
-                    else if (file.nextInt() == 3)
+                    if (file.nextInt() == 3)
                         mat[0][2]++;
-                    else if (file.nextInt() == 4)
+                    if (file.nextInt() == 4)
                         mat[0][3]++;
-                    else if (file.nextInt() == 5)
+                    if (file.nextInt() == 5)
                         mat[0][4]++;
                 }
                 else if (file.nextInt() == 2) {
                     mat[1][5]++;
                     if (file.nextInt() == 1)
                         mat[1][0]++;
-                    else if (file.nextInt() == 2)
+                    if (file.nextInt() == 2)
                         mat[1][1]++;
-                    else if (file.nextInt() == 3)
+                    if (file.nextInt() == 3)
                         mat[1][2]++;
-                    else if (file.nextInt() == 4)
+                    if (file.nextInt() == 4)
                         mat[1][3]++;
-                    else if (file.nextInt() == 5)
+                    if (file.nextInt() == 5)
                         mat[1][4]++;
                 }
-
                 mat[2][0] = mat[0][0] + mat[1][0];
                 mat[2][1] = mat[0][1] + mat[1][1];
                 mat[2][2] = mat[0][2] + mat[1][2];
--- src/Q3/Prog607a.java
+++ src/Q3/Prog607a.java
@@ -4,14 +4,48 @@ import java.util.Scanner;
 public class Prog607a {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("1) Encode Date\n2) Decode Date\t3) Quit");
+        System.out.println("1) Encode Date\n2) Decode Date\n3) Quit");
         System.out.print("Enter Choice: ");
         int choice = input.nextInt();
 
         if (choice == 1) {
+            System.out.print("Enter Date(mm/dd/yy): ");
+            String date = input.next();
+            String code = "";
+            if (date.substring(0, 2).equals("01"))
+                code += "A";
+            if (date.substring(0, 2).equals("02"))
+                code += "B";
+            if (date.substring(0, 2).equals("03"))
+                code += "C";
+            if (date.substring(0, 2).equals("04"))
+                code += "D";
+            if (date.substring(0, 2).equals("05"))
+                code += "E";
+            if (date.substring(0, 2).equals("06"))
+                code += "F";
+            if (date.substring(0, 2).equals("07"))
+                code += "G";
+            if (date.substring(0, 2).equals("08"))
+                code += "H";
+            if (date.substring(0, 2).equals("09"))
+                code += "I";
+            if (date.substring(0, 2).equals("10"))
+                code += "J";
+            if (date.substring(0, 2).equals("11"))
+                code += "K";
+            if (date.substring(0, 2).equals("12"))
+                code += "L";
 
+            System.out.print("The code is: " + code);
         } else if (choice == 2) {
+            System.out.print("Enter Code: ");
+            String code = input.next();
+            int month = 0;
+            int day = 0;
+            int year = 0;
 
+            System.out.println("The date is: " + month + "/" + day + "/" + year);
         } else if (choice == 3) {
 
         }
