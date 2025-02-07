dd1d252f8514a38a4ffea51e6ad06e36ba8958b4
purkapile.s <sjpurkapile@gmail.com>
2025-02-07 11:23:05 -0600

    :)

--- src/Q3/Prog607a.java
+++ src/Q3/Prog607a.java
@@ -47,6 +47,34 @@ public class Prog607a {
             int day = 0;
             int year = 0;
 
+            if (code.substring(0, 1).equals("A"))
+                month = 1;
+            if (code.substring(0, 1).equals("B"))
+                month = 2;
+            if (code.substring(0, 1).equals("C"))
+                month = 3;
+            if (code.substring(0, 1).equals("D"))
+                month = 4;
+            if (code.substring(0, 1).equals("E"))
+                month = 5;
+            if (code.substring(0, 1).equals("F"))
+                month = 6;
+            if (code.substring(0, 1).equals("G"))
+                month = 7;
+            if (code.substring(0, 1).equals("H"))
+                month = 8;
+            if (code.substring(0, 1).equals("I"))
+                month = 9;
+            if (code.substring(0, 1).equals("J"))
+                month = 10;
+            if (code.substring(0, 1).equals("K"))
+                month = 11;
+            if (code.substring(0, 1).equals("L"))
+                month = 12;
+
+
+
+
             System.out.println("The date is: " + month + "/" + day + "/" + year);
         } else if (choice == 3) {
 
