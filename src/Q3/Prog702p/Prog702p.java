586d22bc70a17e8ef1ca2a5ac5d7bb0cc5388ce3
purkapile.s <sjpurkapile@gmail.com>
2025-02-21 11:23:53 -0600

    :)

--- src/Q3/Prog702p/Prog702p.java
+++ src/Q3/Prog702p/Prog702p.java
@@ -9,8 +9,19 @@ public class Prog702p {
             Scanner file = new Scanner(new File("Langdat/prog701g.dat"));
 
             while (file.hasNext()) {
-
+                if (file.nextInt() == 1)
+                    AHHHHHHHHH;
+                else if (file.nextInt() == 2)
+                    AJJJJJJJJJJJJJ;
+                else if (file.nextInt() == 3)
+                    AJJJJJJJJJkKK;
+                else
+                    return;
             }
+
+            System.out.println("Average value of Hicca fur: ");
+            System.out.println("Average number of steps taken by the Wallies: ");
+            System.out.println("Average size of the Beepers' words: ");
 
         } catch (IOException e) {
             System.out.println("Error: " + e);
