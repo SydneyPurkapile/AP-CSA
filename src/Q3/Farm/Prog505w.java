d940a259686ed656cc7da576d8133c11ee481b58
purkapile.s <sjpurkapile@gmail.com>
2025-02-24 11:10:30 -0600

    :)

---
+++ src/Q3/Farm/Prog505w.java
@@ -0,0 +1,24 @@
+package Q3.Farm;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Prog505w {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/prog505w.dat"));
+            List<Animal> animals = new ArrayList<Animal>();
+
+            // TODO: read in animals
+
+            while (file.hasNext()) {
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
