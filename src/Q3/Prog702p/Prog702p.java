12f724d2b8ecef847b4865a41b4b7e264a83ea25
purkapile.s <sjpurkapile@gmail.com>
2025-02-25 11:21:57 -0600

    :)

--- src/Q3/Prog702p/Prog702p.java
+++ src/Q3/Prog702p/Prog702p.java
@@ -4,27 +4,27 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Prog702p {
-    public static void main(String[] args) {
-        try {
-            Scanner file = new Scanner(new File("Langdat/prog701g.dat"));
+    //public static void main(String[] args) {
+        //try {
+           // Scanner file = new Scanner(new File("Langdat/prog701g.dat"));
 
-            while (file.hasNext()) {
-                if (file.nextInt() == 1)
-                    AHHHHHHHHH;
-                else if (file.nextInt() == 2)
-                    AJJJJJJJJJJJJJ;
-                else if (file.nextInt() == 3)
-                    AJJJJJJJJJkKK;
-                else
-                    return;
-            }
+           // while (file.hasNext()) {
+               // if (file.nextInt() == 1)
+                //    AHHHHHHHHH;
+                //else if (file.nextInt() == 2)
+                   // AJJJJJJJJJJJJJ;
+               // else if (file.nextInt() == 3)
+                  //  AJJJJJJJJJkKK;
+                //else
+                    //return;
+           // }
 
-            System.out.println("Average value of Hicca fur: ");
-            System.out.println("Average number of steps taken by the Wallies: ");
-            System.out.println("Average size of the Beepers' words: ");
+          //  System.out.println("Average value of Hicca fur: ");
+           // System.out.println("Average number of steps taken by the Wallies: ");
+           // System.out.println("Average size of the Beepers' words: ");
 
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
-    }
+       // } //catch (IOException e) {
+           // System.out.println("Error: " + e);
+       // }
+    //}
 }
