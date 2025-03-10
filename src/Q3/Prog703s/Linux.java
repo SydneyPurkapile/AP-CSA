77cc3ddafab049cb9ebb752a7a27d5e168a7d97c
purkapile.s <sjpurkapile@gmail.com>
2025-03-10 11:23:17 -0500

    :)

---
+++ src/Q3/Prog703s/Linux.java
@@ -0,0 +1,13 @@
+package Q3.Prog703s;
+
+public class Linux implements Comp {
+    public String getName() {
+        return null;
+    }
+    public int getNumber() {
+        return 0;
+    }
+    public double getValue() {
+        return 0;
+    }
+}
---
+++ src/Q3/Prog703s/Windows.java
@@ -0,0 +1,13 @@
+package Q3.Prog703s;
+
+public class Windows implements Comp {
+    public String getName() {
+        return null;
+    }
+    public int getNumber() {
+        return 0;
+    }
+    public double getValue() {
+        return 0;
+    }
+}
