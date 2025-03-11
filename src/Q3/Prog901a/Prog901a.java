483d2f0f28c25cd9f61c999b2c3c76e115fa7de8
SydneyPurkapile <143824003+SydneyPurkapile@users.noreply.github.com>
2025-03-11 16:55:05 +0000

    :)

---
+++ src/Q3/Prog901a/Prog901a.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog901a {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/prog512h.dat"));
+
+            
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
