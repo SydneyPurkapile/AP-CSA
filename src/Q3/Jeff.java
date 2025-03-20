f6c50293bc853b5c52be81626fcb43859ac8a731
purkapile.s <sjpurkapile@gmail.com>
2025-03-20 11:25:14 -0500

    :)

--- src/Q3/Jeff.java
+++ src/Q3/Jeff.java
@@ -1,7 +1,9 @@
 package Q3;
+import java.util.Scanner;
 
 public class Jeff {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("My name is Jeff");
 
         String favColor = "green";
@@ -10,12 +12,41 @@ public class Jeff {
         String middleName = "James";
         String lastName = "Szelogowski";
         String favClass = "algebra one";
-        String favActivity = "torturing his students";
+        String favActivity = "torturing my students";
         String occupation = "high school teacher";
         String favSport = "golf";
         String worstFear = "touching grass";
         String favPastTime = "playing poker";
 
-        System.out.println("My full name is Jeff " + middleName + " " + lastName);
+        System.out.println("What would you like to ask me?");
+        System.out.println("0) full name");
+        System.out.println("1) favorite color");
+        System.out.println("2) favorite animal");
+        System.out.println("3) favorite food");
+        System.out.println("4) favorite class");
+        System.out.println("5) favorite activity");
+        System.out.println("6) occupation");
+        System.out.println("7) favorite sport");
+        System.out.println("8) worst fear");
+        System.out.println("9) favorite past time");
+
+        int choice = input.nextInt();
+
+        if (choice == 0)
+            System.out.println("Jeff " + middleName + " " + lastName);
+        else if (choice == 1)
+            System.out.println("My favorite color is: " + favColor);
+        else if (choice == 2)
+            System.out.println("My favorite animal is: " + favAnimal);
+        else if (choice == 3)
+            System.out.println("My favorite food is: " + favFood);
+        else if (choice == 4)
+            System.out.println("My favorite class is: " + favClass);
+        else if (choice == 5)
+            System.out.println("My favorite activity is: " + favActivity);
+        else if (choice == 6)
+            System.out.println("My occupation is: " + occupation);
+        else if (choice == 7)
+            System.out.println("My favorite sport is: " + favSport)
     }
 }
